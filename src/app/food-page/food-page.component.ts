90dcd476def8de389612fe689750ec3fd13db3f7
nikilneosoft <nikil.kurian@neosoftmail.com>
2022-07-26 16:53:09 +0530

    styling and adding cart component

--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from '../services/cart/cart.service';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/food';
 
@@ -10,16 +11,20 @@ import { Food } from '../shared/models/food';
 })
 export class FoodPageComponent implements OnInit {
   food!:Food;
-  constructor(private activatedRoute:ActivatedRoute,private foodService:FoodService){ 
+  constructor(private activatedRoute:ActivatedRoute,private foodService:FoodService,
+     private cartService:CartService,private router:Router){ 
     activatedRoute.params.subscribe(params =>{
       if(params['id'])
        this.food = this.foodService.getFoodById(params['id']);
     })
   }
 
-
-
   ngOnInit(): void {
+  }
+
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
   }
 
 }
